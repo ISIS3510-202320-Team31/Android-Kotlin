99785e573b6b99d585a3497f002c8cc571dec939
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2023-09-15 16:53:41 -0500

    Hotfix and MVVM creation

--- app/src/main/java/com/example/hive/view/MainActivity.kt
+++ app/src/main/java/com/example/hive/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.hive
+package com.example.hive.view
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.example.hive.R
 import com.example.hive.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
