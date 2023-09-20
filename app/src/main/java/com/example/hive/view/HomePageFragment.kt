2d89424f44c7274d292ac0e4a2d0ed255c804238
Vanessa Martinez <l.martinezp@uniandes.edu.co>
2023-09-20 15:56:24 -0500

    home page

---
+++ app/src/main/java/com/example/hive/view/HomePageFragment.kt
@@ -0,0 +1,33 @@
+package com.example.hive.view
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hive.R
+import com.example.hive.viewmodel.HomePageViewModel
+
+class HomePageFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomePageFragment()
+    }
+
+    private lateinit var viewModel: HomePageViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home_page, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HomePageViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
