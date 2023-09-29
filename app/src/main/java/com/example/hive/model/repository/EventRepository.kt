53716ede44263aae6e8b68360593f657a67581ce
Vanessa Martinez <l.martinezp@uniandes.edu.co>
2023-09-28 23:18:03 -0500

    QR Scanner

--- app/src/main/java/com/example/hive/model/repository/EventRepository.kt
+++ app/src/main/java/com/example/hive/model/repository/EventRepository.kt
@@ -5,5 +5,6 @@ import com.example.hive.model.di.RetroFitInstance
 class EventRepository() {
 
     suspend fun getEventsR() = RetroFitInstance.api.getEvents()
+    suspend fun getEventsByIdR(id: String) = RetroFitInstance.api.getEventsById(id)
 
 }
