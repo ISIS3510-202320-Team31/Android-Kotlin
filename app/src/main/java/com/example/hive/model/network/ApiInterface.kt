85050568ba97ad213c4b14bb88319f1f9be3a08f
lmartinezp2003 <69610928+lmartinezp2003@users.noreply.github.com>
2023-09-29 15:40:25 -0500

    QR Scanner (#22)

--- app/src/main/java/com/example/hive/model/network/ApiInterface.kt
+++ app/src/main/java/com/example/hive/model/network/ApiInterface.kt
@@ -1,12 +1,16 @@
 package com.example.hive.model.network
 
+import com.example.hive.model.network.responses.EventDetailResponse
 import com.example.hive.model.network.responses.EventResponse
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.Path
 
 interface ApiInterface {
 
     @GET("/events/")
     suspend fun getEvents(): Response<List<EventResponse>>
+
+    @GET("/events/{id}")
+    suspend fun getEventsById(@Path("id") id: String): Response<EventDetailResponse>
 }
---
+++ app/src/main/java/com/example/hive/model/network/responses/EventDetailResponse.kt
@@ -0,0 +1,45 @@
+package com.example.hive.model.network.responses
+
+import com.example.hive.model.models.Creator
+import com.google.gson.annotations.SerializedName
+
+data class EventDetailResponse(
+    @SerializedName("id")
+    var id: String,
+
+    @SerializedName("image")
+    var image: String,
+
+    @SerializedName("name")
+    var name: String,
+
+    @SerializedName("description")
+    var description: String,
+
+    @SerializedName("date")
+    var date: String,
+
+    @SerializedName("place")
+    var place: String,
+
+    @SerializedName("num_participants")
+    var num_participants: Int,
+
+    @SerializedName("category")
+    var category: String,
+
+    @SerializedName("state")
+    var state: Boolean,
+
+    @SerializedName("duration")
+    var duration: Int,
+
+    @SerializedName("creator_id")
+    var creator_id: String,
+
+    @SerializedName("creator")
+    var creator: Creator,
+
+    @SerializedName("participants")
+    var participants: List<String>,
+)
