2e926d65fae2d1c1ccf26449f1e2d4cce056608f
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2023-10-02 14:28:30 -0500

    Feature/implement context aware #32 (#35)
    * smart feature implemented
    
    * fix permission manager

---
+++ app/src/main/java/com/example/hive/view/LocationMonitoringService.kt
@@ -0,0 +1,181 @@
+package com.example.hive.view
+
+import android.Manifest
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.hive.R
+import com.google.android.gms.maps.model.LatLng
+
+class LocationMonitoringService : Service() {
+
+    private lateinit var locationManager: LocationManager
+    private var serviceStarted = false
+
+    private val polygonPoints = listOf(
+        LatLng(4.6053640, -74.0666571),
+        LatLng(4.6029762, -74.0677709),
+        LatLng(4.6022922, -74.0661728),
+        LatLng(4.6009240, -74.0667842),
+        LatLng(4.5992985, -74.0660603),
+        LatLng(4.5986837, -74.0635839),
+        LatLng(4.5990685, -74.0611064),
+        LatLng(4.6024247, -74.0607632),
+        LatLng(4.6053533, -74.0666619),
+        LatLng(4.6053640, -74.0666571)
+    )
+
+    private val locationListener = LocationListener { location ->
+        val userLatLng = LatLng(location.latitude, location.longitude)
+        println("User location: $userLatLng")
+        val isInsidePolygon = isPointInPolygon(userLatLng, polygonPoints)
+        if (isInsidePolygon) {
+            val notification = createNotification()
+            startForeground(1, notification)
+            sendNotification()
+        }
+    }
+
+    private fun isPointInPolygon(point: LatLng, polygon: List<LatLng>): Boolean {
+        val x = point.latitude
+        val y = point.longitude
+        var isInside = false
+
+        var j = polygon.size - 1
+        for (i in polygon.indices) {
+            val xi = polygon[i].latitude
+            val yi = polygon[i].longitude
+            val xj = polygon[j].latitude
+            val yj = polygon[j].longitude
+
+            val intersect =
+                ((yi > y) != (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi)
+            if (intersect) isInside = !isInside
+
+            j = i
+        }
+        return isInside
+    }
+
+    private fun createNotification(): Notification {
+        val channelId = "my_channel_id"
+
+        // Create an explicit intent for the MainActivity
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+        // Create a PendingIntent for launching the MainActivity
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        // Build the notification using NotificationCompat.Builder
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle("Estas muy cerca a Uniandes!")
+            .setContentText("Descubre los eventos que estan esperando por ti")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        // Check if the device is running Android Oreo (API 26) or higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "My Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        return notificationBuilder.build()
+    }
+
+    private fun sendNotification() {
+        val channelId = "my_channel_id"
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle("Estas muy cerca a Uniandes!")
+            .setContentText("Descubre los eventos que estan esperando por ti")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "My Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(1, notificationBuilder.build())
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (!serviceStarted) {
+            locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return START_STICKY
+            }
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                MIN_TIME_BETWEEN_UPDATES,
+                MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                locationListener
+            )
+            serviceStarted = true
+        }
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        locationManager.removeUpdates(locationListener)
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "my_channel_id"
+        private const val NOTIFICATION_ID = 1
+        private const val MIN_TIME_BETWEEN_UPDATES = 10000L
+        private const val MIN_DISTANCE_CHANGE_FOR_UPDATES = 100.0f
+    }
+}
--- app/src/main/java/com/example/hive/view/MainActivity.kt
+++ app/src/main/java/com/example/hive/view/MainActivity.kt
@@ -1,18 +1,53 @@
 package com.example.hive.view
 
 import android.content.Intent
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.graphics.Color
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import android.view.Menu
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.hive.R
 import com.example.hive.databinding.ActivityMainBinding
 import com.example.hive.model.adapters.SessionManager
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
+import kotlin.properties.Delegates
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var sessionManager: SessionManager
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var hasLocationStarted = false
+
+    private val polygonPoints = listOf(
+        LatLng(4.6053640, -74.0666571),
+        LatLng(4.6029762, -74.0677709),
+        LatLng(4.6022922, -74.0661728),
+        LatLng(4.6009240, -74.0667842),
+        LatLng(4.5992985, -74.0660603),
+        LatLng(4.5986837, -74.0635839),
+        LatLng(4.5990685, -74.0611064),
+        LatLng(4.6024247, -74.0607632),
+        LatLng(4.6053533, -74.0666619),
+        LatLng(4.6053640, -74.0666571)
+    )
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +57,54 @@ class MainActivity : AppCompatActivity() {
 
         sessionManager = SessionManager(this)
 
+        var notificationSent = sessionManager.getNotification()
+
         if (!isUserLoggedIn()) {
             navigateToLogin()
+        } else if (!notificationSent) {
+            // Send the notification
+            sendNotification()
+
+            // Set the notification status to "sent" in shared preferences
+            sessionManager.saveNotification(true)
         }
 
         replaceFragment(HomePageFragment())
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // Request location permission
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // Ask for background location permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                        LOCATION_PERMISSION_REQUEST_CODE
+                    )
+                }
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
+
+        if (!hasLocationStarted) {
+            hasLocationStarted = true
+            startLocationMonitoringService()
+        }
 
         binding.bottomNavigation.setOnItemSelectedListener {
             when(it.itemId) {
@@ -78,4 +156,89 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
         finish()
     }
+
+    private fun checkUserLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            location?.let {
+                val userLatLng = LatLng(location.latitude, location.longitude)
+                val isInsidePolygon = isPointInPolygon(userLatLng, polygonPoints)
+                if (isInsidePolygon) {
+                }
+            }
+        }
+    }
+    private fun isPointInPolygon(point: LatLng, polygon: List<LatLng>): Boolean {
+        val x = point.latitude
+        val y = point.longitude
+        var isInside = false
+
+        var j = polygon.size - 1
+        for (i in polygon.indices) {
+            val xi = polygon[i].latitude
+            val yi = polygon[i].longitude
+            val xj = polygon[j].latitude
+            val yj = polygon[j].longitude
+
+            val intersect =
+                ((yi > y) != (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi)
+            if (intersect) isInside = !isInside
+
+            j = i
+        }
+        return isInside
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                checkUserLocation()
+            }
+        }
+    }
+
+    private fun startLocationMonitoringService() {
+        val intent = Intent(this, LocationMonitoringService::class.java)
+        startService(intent)
+    }
+
+    private fun sendNotification() {
+        val channelId = "123"
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_star)
+            .setContentTitle("Bienvenido a Hive!")
+            .setContentText("La mejor app de eventos")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Welcome Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(1, notificationBuilder.build())
+    }
+
 }
